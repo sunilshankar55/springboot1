1a41525a363051e3a10980bd3e0a3ba64d4af676
sunilshankar55 <120124490+sunilshankar55@users.noreply.github.com>
2024-04-16 18:26:11 +0530

    Add files via upload

---
+++ vem1/src/main/java/com/example/vem1/Vem1Application.java
@@ -0,0 +1,13 @@
+package com.example.vem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vem1Application.class, args);
+	}
+
+}
---
+++ vem1/src/test/java/com/example/vem1/Vem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
